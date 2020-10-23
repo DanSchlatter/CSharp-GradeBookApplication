81263af83c7288f8ef782940bcea294dcc3ad168
DanSchlatter <danschlatter@outlook.com>
2020-10-23 13:26:56 -0600

    Weighted Letter Grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -10,6 +11,27 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name): base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int studentCount = Students.Count;
+            char letterGrade = 'F';
+            if (studentCount < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                int higherGradeCount = Students.Where(s => s.AverageGrade >= averageGrade).Count();
+                double percentile = (double)higherGradeCount / (double)studentCount;
+
+                if (percentile <= 0.2d) letterGrade = 'A';
+                if (percentile <= 0.4d && percentile > 0.2d) letterGrade = 'B';
+                if (percentile <= 0.6d && percentile > 0.4d) letterGrade = 'C';
+                if (percentile <= 0.8d && percentile > 0.6d) letterGrade = 'D';
+            }
+            return letterGrade;
         }
     }
 }
